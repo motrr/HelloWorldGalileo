c0baca33ede74a0602c85254dde8bbf86094094f
Chris Harding <chris@motrr.com>
2013-02-25 12:14:37 -0800

    Created emtpy XCode project

---
+++ HelloWorldGalileo/HelloWorldGalileo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorldGalileo
+//
+//  Created by Chris Harding on 2/25/13.
+//  Copyright (c) 2013 Chris Harding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloWorldGalileo/HelloWorldGalileo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloWorldGalileo
+//
+//  Created by Chris Harding on 2/25/13.
+//  Copyright (c) 2013 Chris Harding. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
