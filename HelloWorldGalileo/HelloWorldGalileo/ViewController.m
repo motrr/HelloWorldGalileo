163945a17598813b2e2d346fd907f71d91588a97
Chris Harding <chris@motrr.com>
2013-02-25 18:03:41 -0800

    Attempting to initialise connection to Galileo accessory

--- HelloWorldGalileo/HelloWorldGalileo/ViewController.m
+++ HelloWorldGalileo/HelloWorldGalileo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <GalileoControl/GalileoControl.h>
 
 @interface ViewController ()
 
@@ -18,6 +19,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[Galileo sharedGalileo] waitForConnection];
 }
 
 - (void)didReceiveMemoryWarning
